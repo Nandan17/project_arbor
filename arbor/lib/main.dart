134b614db92279b2e40f0ddf5d42cc5390f3a958
Nandan17 <vgnandan02@gmail.com>
2022-04-26 20:12:18 +0530

    third commit

--- arbor/lib/main.dart
+++ arbor/lib/main.dart
@@ -1,30 +1,61 @@
-import 'package:arbor/providers/counter_provider.dart';
-import 'package:arbor/views/home_screen.dart';
+import 'package:arbor/views/login_view.dart';
+import 'package:arbor/views/register_view.dart';
+import 'package:arbor/views/verify_email_view.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'firebase_options.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => Counter()),
-      ],
-      child: MyApp(),
-    ),
+   MaterialApp(
+     title: 'Arbor',
+     theme: ThemeData(
+       primaryColor: Colors.green,
+     ),
+     home: const HomePage(),
+     routes: {
+       '/login/':(context) => LoginView(),
+       '/register/':(context) => RegisterView(),
+       '/verify-email-view/':(context) => VerifyEmailView(),
+     },
+   )
   );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class HomePage extends StatelessWidget {
+  const HomePage({ Key? key }) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      initialRoute: '/',
-      routes: {
-        '/': (context) => MyHomePage(),
-      },
+    return FutureBuilder(
+      future: Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
+      ),
+     builder: (context, snapshot) {
+           switch (snapshot.connectionState){
+             case ConnectionState.done:
+                final user = FirebaseAuth.instance.currentUser;
+                if(user != null){
+                  
+                  if(user.emailVerified){
+                     //devtools.log('email varified');
+                     //return const LoginView();
+                     print('Email is verified');
+                   }else{
+                     return const VerifyEmailView();
+                   }
+                }else{
+                    return const LoginView();
+                }
+               
+               return const Text('Done');  
+               default:
+                  return const CircularProgressIndicator();
+          }
+          
+        },
     );
   }
 }
---
+++ arbor/lib/views/verify_email_view.dart
@@ -0,0 +1,41 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class VerifyEmailView extends StatefulWidget {
+  const VerifyEmailView({ Key? key }) : super(key: key);
+
+  @override
+  State<VerifyEmailView> createState() => _VerifyEmailViewState();
+}
+
+class _VerifyEmailViewState extends State<VerifyEmailView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Varify the email'),),
+      body: Column(
+        children: [
+          const Text('We\'ve sent to you an email varification. Please oprn it to varify your account'),
+          const Text('If you haven\'t received a varification email yet, press the button below'),
+          TextButton(
+            onPressed: () async {
+              final user = FirebaseAuth.instance.currentUser;
+              await user?.sendEmailVerification();
+              },
+              child:const Text('Send email varification'),
+           ),
+            TextButton(
+             onPressed: () {
+               //await FirebaseAuth.instance.logout();
+               Navigator.of(context).pushNamedAndRemoveUntil(
+                 '/register/',
+                 (route) => false,
+                 );
+             },
+             child: const Text("Restart"),
+             )
+        ],
+      ),
+    );
+  }
+}
